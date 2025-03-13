4974fd15c3428f5e555f86c9f57e13f77ccf1664
Arnaud Briche <briche.arnaud@gmail.com>
2025-03-13 18:39:21 +0100

    chore: log init vars

--- internal/pipeline/init.go
+++ internal/pipeline/init.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/agnosticeng/agt/internal/ch"
 	"github.com/agnosticeng/agt/internal/engine"
+	slogctx "github.com/veqryn/slog-context"
 )
 
 type InitConfig struct {
@@ -23,6 +24,8 @@ func Init(
 	vars map[string]interface{},
 	conf InitConfig,
 ) (map[string]any, error) {
+	var logger = slogctx.FromCtx(ctx)
+
 	for i, query := range conf.Queries {
 		rows, _, err := ch.QueryFromTemplate(ctx, engine, tmpl, query.Path, vars)
 
@@ -31,6 +34,7 @@ func Init(
 		}
 
 		if i == (len(conf.Queries)-1) && len(rows) > 0 {
+			logger.Info("init vars", mapToSlice(rows[len(rows)-1])...)
 			return rows[len(rows)-1], nil
 		}
 
